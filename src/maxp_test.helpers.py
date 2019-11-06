61cafb8ff7e726489e1e7494b1ddebf6aba3877c
Pablo Fernandez <fernandezpablo85@gmail.com>
2019-11-06 13:22:05 -0300

    extra prop

--- src/maxp_test.helpers.py
+++ src/maxp_test.helpers.py
@@ -11,3 +11,11 @@
 # @given(lists(integers(), min_size=2))
 # def test_max_product_slow_oracle(ns):
 #     assert max_product(ns) == slow_max_product(ns)
+
+
+# @given(lists(integers(), min_size=2))
+# def test_max_product_slow_oracle(ns):
+#     times_two = [n * 2 for n in ns]
+#     p = max_product(ns)
+#     px2 = max_product(times_two)
+#     assert p * 4 == px2
