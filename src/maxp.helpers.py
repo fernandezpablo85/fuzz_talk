db33d79b9e4ce795068945f3a4554b81641a5e9c
Pablo Fernandez <fernandezpablo85@gmail.com>
2019-11-09 20:06:16 -0300

    remove slow_max_product stuff

--- src/maxp.helpers.py
+++ src/maxp.helpers.py
@@ -6,14 +6,3 @@
 #     largest, second_largest = sorted_ns[-2:]
 #     smallest, second_smallest = sorted_ns[:2]
 #     return max([largest * second_largest, smallest * second_smallest])
-
-
-# def slow_max_product(ns):
-#     from itertools import combinations
-#     import math
-
-#     largest = -math.inf
-#     for (a, b) in combinations(ns, 2):
-#         if a * b > largest:
-#             largest = a * b
-#     return largest
--- src/maxp_test.helpers.py
+++ src/maxp_test.helpers.py
@@ -7,16 +7,3 @@
 #     p = max_product(ns)
 #     one, two = np.random.choice(ns, 2)
 #     assert p >= (one * two)
-
-
-# @given(lists(integers(), min_size=2))
-# def test_max_product_slow_oracle(ns):
-#     assert max_product(ns) == slow_max_product(ns)
-
-
-# @given(lists(integers(), min_size=2))
-# def test_max_product_slow_oracle(ns):
-#     times_two = [n * 2 for n in ns]
-#     p = max_product(ns)
-#     px2 = max_product(times_two)
-#     assert p * 4 == px2
