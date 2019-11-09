b8f37bfc8c362d73f655f5e13fef0b2a15bb411d
Pablo Fernandez <fernandezpablo85@gmail.com>
2019-11-09 20:05:09 -0300

    maxp docstring

--- src/maxp.py
+++ src/maxp.py
@@ -1,4 +1,8 @@
 def max_product(ns):
+    """Given a list of numbers, return the maximum product possible between two.
+
+    Example: [10,2,3,4] => 4 * 10 => 40
+    """
     if len(ns) < 2:
         return None
 
