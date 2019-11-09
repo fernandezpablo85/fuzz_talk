b58cd62693c33cc74784c2dd644b34734c669d09
Pablo Fernandez <fernandezpablo85@gmail.com>
2019-11-09 20:13:44 -0300

    suffix docstring

--- src/suffix.py
+++ src/suffix.py
@@ -1,4 +1,8 @@
 def remove_suffix(suffix, string):
+    """Removes suffix for string if present. If not, returns string as is
+
+    Example: remove_suffix(".com", "example.com") => "example"
+    """
     if string.endswith(suffix):
         ls = len(suffix)
         return string[:-ls]
