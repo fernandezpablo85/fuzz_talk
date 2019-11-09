da697f6f650b22e32d127e726f7a528b281b8fd3
Pablo Fernandez <fernandezpablo85@gmail.com>
2019-11-09 20:14:26 -0300

    better test example

--- src/suffix_test.py
+++ src/suffix_test.py
@@ -4,4 +4,4 @@ from suffix import remove_suffix as remove
 def test_remove_suffix():
     assert "pablo" == remove(suffix="@gmail.com", string="pablo@gmail.com")
     assert "google" == remove(suffix=".com.ar", string="google.com.ar")
-    assert "google.com.ar" == remove(suffix=".not. .suffix", string="google.com.ar")
+    assert "google.com.ar" == remove(suffix=".not.a.suffix", string="google.com.ar")
