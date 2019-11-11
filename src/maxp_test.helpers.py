7c10dca0f66b5af863018c8d9949f00814fb6634
Pablo Fernandez <fernandezpablo85@gmail.com>
2019-11-10 22:20:35 -0300

    add np dep, round numpy int32

--- src/maxp_test.helpers.py
+++ src/maxp_test.helpers.py
@@ -1,9 +1,11 @@
 # from hypothesis import given
 # from hypothesis.strategies import integers, lists
 
+# import numpy as np
+
 
 # @given(lists(integers(), min_size=2))
 # def test_max_product_largest_than_any_two(ns):
 #     p = max_product(ns)
 #     one, two = np.random.choice(ns, 2)
-#     assert p >= (one * two)
+#     assert p >= (int(one) * int(two))
